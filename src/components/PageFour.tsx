464dc631093538a74aaec12af95f062711cee334
dennistonzo08 <dennistonzo08@gmail.com>
2025-06-24 03:35:18 +0800

    Update: Page One

---
+++ src/components/PageFour.tsx
@@ -0,0 +1,59 @@
+function PageFour() {
+  return (
+    <section className="h-screen cocky_brown">
+      <h1>LETS TALK!</h1>
+
+      <div>
+        <div>
+          <img src="" alt="email" />
+          <p>Send me an email:</p>
+          <p>dennistonzo08@gmail.com</p>
+        </div>
+
+        <div>
+          <img src="" alt="linkedin" />
+          <p>My LinkedIn Account:</p>
+          <p>/in/tonzo</p>
+        </div>
+
+        <div>
+          <img src="" alt="github" />
+          <p>My Github Account:</p>
+          <p>@dennistonzo08</p>
+        </div>
+
+        <div>
+          <img src="" alt="facebook" />
+          <p>My Facebook Account:</p>
+          <p>/dennis.john.45442</p>
+        </div>
+
+        <div>
+          <img src="" alt="instagram" />
+          <p>My Instagram Account:</p>
+          <p>@sowsitrash</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h1>Powered By:</h1>
+          <div>
+            <img src="" alt="vite" />
+          </div>
+          <div>
+            <img src="" alt="react" />
+          </div>
+          <div>
+            <img src="" alt="css" />
+          </div>
+        </div>
+        <div>
+          <p>Designed and Maintained By: Dennis John Tonzo</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PageFour;
